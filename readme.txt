8744b23d8cc0d0716b4bd89e1999f361c952264b
arnesonium <arnesonium@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-23 23:52:27 +0000

    Cedexis Radar plugin for WordPress
    Quickly and easily add Cedexis Radar tracking to any WordPress site.
    
    git-svn-id: https://plugins.svn.wordpress.org/wp-cedexis/trunk@1074510 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,62 @@
+=== OpenPGP Form Encryption for WordPress ===
+Contributors: arnesonium
+Tags: cedexis, webperf, cdn, performance, monitoring, analytics
+Requires at least: 4.0
+Tested up to: 4.1
+Stable tag: v0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RXC68YPEATPUU
+
+Add Cedexis Radar monitoring to your WordPress blogs.
+
+== Description ==
+
+This plugin adds [Cedexis Radar](http://www.cedexis.com/radar/) monitoring to your WordPress
+blog. Cedexis Radar is a community-driven real user monitoring (RUM)
+tool used to measure website performance. Radar data can be used to
+feed the Cedexis Openmix decision-making engine, and also contributes
+to a holistic view of network health. 
+
+This plugin relies on the =wp_footer= action, which most themes
+support. If your theme doesn't support it, please contact the theme's
+author.
+
+= Usage =
+
+Once configured, this plugin automatically enables Radar tracking
+across your WordPress site.
+
+== Installation ==
+
+To install this plugin, follow these directions:
+
+1. Download the latest zip file.
+1. Next, load up your WordPress blog’s dashboard, and go to **Plugins > Add New**.
+1. Upload the zip file.
+1. Click **Activate**.
+
+You will also need a free Cedexis Portal account.
+
+1. Sign up at https://portal.cedexis.com/public/signup.html
+1. In the Cedexis Portal, go to **Configure PLT URLs** and add the
+   base URL of your WordPress site.
+1. Also in the Cedexis Portal, click on **Radar Tag**.
+1. Line 3 of the JavaScript code for the Radar Tag begins with "a.src"
+   and contains the tag ID that the plugin needs. For instance, in
+   "//radar.cedexis.com/1/11917/radar.js" the tag is "11917".
+1. Enter the tag ID from the Radar Tag into the plugin's configuration
+   screen.
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+== Changelog ==
+= 0.1 =
+* Development pre-release.
+
+== Upgrade Notice ==
+
+
---
+++ wp-cedexis.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @package WP-Cedexis
+ */
+/*
+Plugin Name: Cedexis Radar for WordPress
+Description: Track site performance with Cedexis Radar
+Version: 0.1
+Author: Erik L. Arneson
+Author URI: http://www.arnesonium.com/
+License: GPLv2 or later
+
+    Copyright 2015 Erik L. Arneson (email : earneson@arnesonium.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+defined('ABSPATH') or die("No script kiddies please!");
+
+function wp_cedexis_add_tag () {
+    $radarTagId = get_option('cedexis_radar_tag_id');
+
+    // If the plugin isn't configured, fail silently.
+    if (false == $radarTagId) {
+        return;
+    }
+
+    $radarTag =<<< EOT
+<script> 
+(function(a,b,c,d,e){function f(){var a=b.createElement("script");a.async=!0;
+a.src="//radar.cedexis.com/1/%s/radar.js";b.body.appendChild(a)}/\bMSIE 6/i
+.test(a.navigator.userAgent)||(a[c]?a[c](e,f,!1):a[d]&&a[d]("on"+e,f))})
+(window,document,"addEventListener","attachEvent","load");
+</script> 
+EOT;
+
+    printf($radarTag, $radarTagId);
+}
+add_action('wp_footer', 'wp_cedexis_add_tag');
+
+function wp_cedexis_register_settings () {
+    register_setting('wp-cedexis-settings', 'cedexis_radar_tag_id');
+}
+
+function wp_cedexis_settings_menu () {
+?>
+<div class="wrap"><div id="icon-tools" class="icon32"></div>
+<img src="<?php echo plugins_url('img/logo-cedexis.png', __FILE__); ?>" style="float:right">
+<h2>Cedexis Radar Configuration</h2>
+<form method="post" action="options.php">
+<?php 
+settings_fields('wp-cedexis-settings');
+do_settings_sections('wp-cedexis-settings'); 
+?>
+  <table class="form-table">
+    <tr valign="top">
+      <th scope="row">Radar Tag ID</th>
+      <td>
+<input type="text" name="cedexis_radar_tag_id" value="<?php echo esc_attr(get_option('cedexis_radar_tag_id')); ?>" />
+<p>To find your Radar tag ID, log into the <a target="_top" href="https://portal.cedexis.com">Cedexis Portal</a> and 
+go to the <b>Radar Tag</b> page. On line 3 of the code, you will see a number before the "radar.js" script. Enter that 
+number into this box.</p>
+</td>
+    </tr>
+  </table>
+  <?php submit_button(); ?>
+</form>
+</div>
+<?php
+}
+
+function wp_cedexis_register_settings_menu () {
+    add_submenu_page( 
+        'options-general.php',
+        'Cedexis Radar',
+        'Cedexis Radar',
+        'manage_options', 
+        'wp_cedexis_radar', 
+        'wp_cedexis_settings_menu' 
+    );
+}
+
+add_action('admin_menu', 'wp_cedexis_register_settings_menu');
+add_action('admin_init', 'wp_cedexis_register_settings');
